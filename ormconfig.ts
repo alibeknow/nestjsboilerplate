fda4307df67a4057c9148842178b4927e6600ad8
alibek.nauryzbayev@sdc.kz <alibek.nauryzbayev@sdc.kz>
2021-07-09 15:37:22 +0600

    add migrations

--- ormconfig.ts
+++ ormconfig.ts
@@ -4,11 +4,11 @@ import { SnakeNamingStrategy } from './src/snake-naming.strategy';
 
 module.exports = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST || '127.0.0.1',
+  port: +process.env.DB_PORT || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_DATABASE || 'postgres',
   namingStrategy: new SnakeNamingStrategy(),
   entities: ['src/modules/**/*.entity{.ts,.js}'],
   migrations: ['src/migrations/*{.ts,.js}'],
