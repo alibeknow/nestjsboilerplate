c1ce0ee08f895ef67e9cd88b0f3e74afe9eeedbd
alibek.nauryzbayev@sdc.kz <alibek.nauryzbayev@sdc.kz>
2021-07-09 15:24:21 +0600

    clean code

--- src/modules/signature/signature.service.ts
+++ src/modules/signature/signature.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 
-import { AuthService } from '../auth/auth.service';
 import type { SignatureDto } from './dto/signatureDto';
 
 @Injectable()
