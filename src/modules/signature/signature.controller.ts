43957d4d15fcbd4640d7de6670ff18dcf7266a11
alibek.nauryzbayev@sdc.kz <alibek.nauryzbayev@sdc.kz>
2021-07-09 15:04:49 +0600

    add

--- src/modules/signature/signature.controller.ts
+++ src/modules/signature/signature.controller.ts
@@ -19,12 +19,14 @@ export class SignatureController {
     const headers = {
       'Content-Type': 'application/json',
     };
-    const result = await axios.post('http://10.0.85.60:14579', signatureData, {
+    const {
+      data: { result },
+    } = await axios.post('http://10.0.85.60:14579', signatureData, {
       headers,
     });
     return {
-      valid: result.data.result.valid,
-      subject: result.data.result.cert.chain[0],
+      valid: result.valid,
+      subject: result.cert.chain[0],
     };
   }
 }
