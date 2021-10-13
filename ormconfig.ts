7c3b2514d0694ed2253a7ae8ab588e087f9ec60c
Aibat <aibat.zhaisanov@gmail.com>
2021-10-13 13:14:14 +0600

    Edit ormconfig.ts

--- ormconfig.ts
+++ ormconfig.ts
@@ -4,11 +4,11 @@ import { SnakeNamingStrategy } from './src/snake-naming.strategy';
 
 module.exports = {
   type: 'postgres',
-  host: process.env.DB_HOST || '127.0.0.1',
+  host: process.env.DB_HOST || '10.0.124.132',
   port: +process.env.DB_PORT || 5432,
-  username: process.env.DB_USERNAME || 'postgres',
-  password: process.env.DB_PASSWORD || 'postgres',
-  database: process.env.DB_DATABASE || 'postgres',
+  username: process.env.DB_USERNAME || 'kaztoll_user',
+  password: process.env.DB_PASSWORD || 'VW9z7FNcAv',
+  database: process.env.DB_DATABASE || 'kaztoll_enterprise',
   namingStrategy: new SnakeNamingStrategy(),
   entities: ['src/modules/**/*.entity{.ts,.js}'],
   migrations: ['src/migrations/*{.ts,.js}'],
